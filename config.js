4eb6e452160fd1ae3369d5d48fcf28549380c2c6
Gabriel <gabrielenriquecas@gmail.com>
2023-09-09 22:09:14 -0400

    updated

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const PAGE_URL = process.env.NODE_ENV === 'production' ? 'placeholder' : 'http://localhost:3001';
+const PAGE_URL = process.env.NODE_ENV === 'production' ? 'https://contactsapp-0514.onrender.com' : 'http://localhost:3001';
 const MONGO_URI = process.env.NODE_ENV === 'production' ? process.env.MONGO_URI_PROD : process.env.MONGO_URI_TEST;
 
 module.exports = { PAGE_URL, MONGO_URI };
