4c207ca5387c10a0d93752fcd2345cd74c202ea9
Gabriel <gabrielenriquecas@gmail.com>
2023-10-14 16:39:02 -0400

    fixed

--- views/components/nav.js
+++ views/components/nav.js
@@ -17,9 +17,9 @@ const createNavHome = () => {
                 <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
             </svg> -->
 
-        <label class="hamburger p-2 rounded-lg md:hidden">
+        <label class="hamburger rounded-lg md:hidden">
             <input type="checkbox">
-            <svg viewBox="0 0 32 32" class="red">
+            <svg viewBox="0 0 32 32" class="p-2">
                 <path class="line line-top-bottom" d="M27 10 13 10C10.8 10 9 8.2 9 6 9 3.5 10.8 2 13 2 15.2 2 17 3.8 17 6L17 26C17 28.2 18.8 30 21 30 23.2 30 25 28.2 25 26 25 23.8 23.2 22 21 22L7 22" stroke="black"></path>
                 <path class="line" d="M7 16 27 16"></path>
             </svg>
@@ -52,9 +52,18 @@ const createNavSignup = () => {
 
         <!-- mobile version -->
 
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10 p-2 rounded-lg hover:bg-zinc-300 dark:text-zinc-50 hover:dark:bg-slate-700 md:hidden cursor-pointer">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-        </svg>
+        <!--<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10 p-2 rounded-lg hover:bg-zinc-300 dark:text-zinc-50 hover:dark:bg-slate-700 md:hidden cursor-pointer">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+            </svg> -->
+
+        <label class="hamburger rounded-lg md:hidden">
+            <input type="checkbox">
+            <svg viewBox="0 0 32 32" class="p-2">
+                <path class="line line-top-bottom" d="M27 10 13 10C10.8 10 9 8.2 9 6 9 3.5 10.8 2 13 2 15.2 2 17 3.8 17 6L17 26C17 28.2 18.8 30 21 30 23.2 30 25 28.2 25 26 25 23.8 23.2 22 21 22L7 22" stroke="black"></path>
+                <path class="line" d="M7 16 27 16"></path>
+            </svg>
+        </label>
+        
 
         <!-- tablet/pc version -->
 
@@ -65,7 +74,7 @@ const createNavSignup = () => {
           <!-- menu mobile -->
 
             <div class="fixed bg-slate-800 bg-opacity-90 top-16 right-0 hidden left-0 bottom-0 justify-center items-center flex-col text-slate-50 gap-4">
-                <a href="/login/" class="bg-[#39BBBD] hover:bg-[#429fa0] dark:text-zinc-900 rounded-lg p-4">Iniciar Sesión</a>
+                <a href="/login/" class="bg-[#f1fcfb] hover:bg-[#d0f7f3] text-zinc-900 dark:text-zinc-900 rounded-lg p-4 w-32 text-center">Iniciar Sesión</a>
             </div>          
     </div>
     `;
@@ -80,9 +89,18 @@ const createNavLogin = () => {
 
         <!-- mobile version -->
 
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10 p-2 rounded-lg hover:bg-zinc-300 dark:text-zinc-50 hover:dark:bg-slate-700 md:hidden cursor-pointer">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-        </svg>
+        <!--<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10 p-2 rounded-lg hover:bg-zinc-300 dark:text-zinc-50 hover:dark:bg-slate-700 md:hidden cursor-pointer">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+            </svg> -->
+
+        <label class="hamburger rounded-lg md:hidden">
+            <input type="checkbox">
+            <svg viewBox="0 0 32 32" class="p-2">
+                <path class="line line-top-bottom" d="M27 10 13 10C10.8 10 9 8.2 9 6 9 3.5 10.8 2 13 2 15.2 2 17 3.8 17 6L17 26C17 28.2 18.8 30 21 30 23.2 30 25 28.2 25 26 25 23.8 23.2 22 21 22L7 22" stroke="black"></path>
+                <path class="line" d="M7 16 27 16"></path>
+            </svg>
+        </label>
+        
 
         <!-- tablet/pc version -->
 
@@ -93,12 +111,40 @@ const createNavLogin = () => {
           <!-- menu mobile -->
 
             <div class="fixed bg-slate-800 bg-opacity-90 top-16 right-0 hidden left-0 bottom-0 justify-center items-center flex-col text-slate-50 gap-4">
-                <a href="/signup/" class="bg-[#39BBBD] hover:bg-[#429fa0] dark:text-zinc-900 rounded-lg p-4">Registro</a>
+                <a href="/signup/" class="bg-[#39BBBD] hover:bg-[#429fa0] dark:text-zinc-900 rounded-lg p-4 w-32 text-center">Registro</a>
             </div>          
     </div>
     `;
 };
 const createNavContacts = () => {
+    // navbar.innerHTML = `
+    // <div class="max-w-7xl h-16 mx-auto flex items-center px-4 text-zinc-900 dark:text-slate-50 justify-between ">
+
+    //     <a class="font-bold text-xl  dark:text-zinc-50 flex items-center gap-2" href="/" >
+    //         <img src="/images/contact-list-logo.jpg" class="h-8 w-8 rounded-md"> <div>Contacts<span class="text-[#39BBBD]">App</span></div></img>
+    //     </a>
+
+    //     <!-- mobile version -->
+
+    //     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10 p-2 rounded-lg hover:bg-zinc-300 dark:text-zinc-50 hover:dark:bg-slate-700 md:hidden cursor-pointer">
+    //         <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+    //     </svg>
+
+    //     <!-- tablet/pc version -->
+
+    //       <div class="hidden md:flex flex-row gap-4">
+    //         <button id="close-btn" class="hover:underline dark:text-slate-50">Cerrar sesión</button>
+    //         <span class="updating hidden"></span>
+    //       </div>
+
+    //       <!-- menu mobile -->
+
+    //         <div class="fixed bg-slate-800 bg-opacity-90 top-16 right-0 hidden left-0 bottom-0 justify-center items-center flex-col text-slate-50 gap-4">
+    //             <button id="close-btn" class="bg-[#39BBBD] hover:bg-[#429fa0] rounded-lg p-4">Cerrar sesión</button>
+    //             <span class="updating hidden"></span>
+    //         </div>
+    // </div>
+    // `;
     navbar.innerHTML = `
     <div class="max-w-7xl h-16 mx-auto flex items-center px-4 text-zinc-900 dark:text-slate-50 justify-between ">
 
@@ -108,23 +154,32 @@ const createNavContacts = () => {
 
         <!-- mobile version -->
 
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10 p-2 rounded-lg hover:bg-zinc-300 dark:text-zinc-50 hover:dark:bg-slate-700 md:hidden cursor-pointer">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-        </svg>
+        <!--<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10 p-2 rounded-lg hover:bg-zinc-300 dark:text-zinc-50 hover:dark:bg-slate-700 md:hidden cursor-pointer">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+            </svg> -->
+
+        <label class="hamburger rounded-lg md:hidden">
+            <input type="checkbox">
+            <svg viewBox="0 0 32 32" class="p-2">
+                <path class="line line-top-bottom" d="M27 10 13 10C10.8 10 9 8.2 9 6 9 3.5 10.8 2 13 2 15.2 2 17 3.8 17 6L17 26C17 28.2 18.8 30 21 30 23.2 30 25 28.2 25 26 25 23.8 23.2 22 21 22L7 22" stroke="black"></path>
+                <path class="line" d="M7 16 27 16"></path>
+            </svg>
+        </label>
+        
 
         <!-- tablet/pc version -->
 
-          <div class="hidden md:flex flex-row gap-4">
-            <button id="close-btn" class="hover:underline dark:text-slate-50">Cerrar sesión</button>
-            <span class="updating hidden"></span>
-          </div>
+               <div class="hidden md:flex flex-row gap-4">
+                 <button id="close-btn" class="hover:underline dark:text-slate-50">Cerrar sesión</button>
+                 <span class="updating hidden"></span>
+               </div>
 
           <!-- menu mobile -->
 
-            <div class="fixed bg-slate-800 bg-opacity-90 top-16 right-0 hidden left-0 bottom-0 justify-center items-center flex-col text-slate-50 gap-4">
-                <button id="close-btn" class="bg-[#39BBBD] hover:bg-[#429fa0] rounded-lg p-4">Cerrar sesión</button>
-                <span class="updating hidden"></span>
-            </div>          
+                <div class="fixed bg-slate-800 bg-opacity-90 top-16 right-0 hidden left-0 bottom-0 justify-center items-center flex-col text-slate-50 gap-4">
+                    <button id="close-btn" class="bg-[#39BBBD] hover:bg-[#429fa0] rounded-lg p-4">Cerrar sesión</button>
+                    <span class="updating hidden"></span>
+                </div>          
     </div>
     `;
 };
